eb47414c7a49773fe55f20dea78945483e7830e9
HumaAltaf5 <huma.altaf@gmail.com>
2022-02-15 17:52:03 +0000

    Initial commit

---
+++ src/main/java/com/huma/aplab1/Aplab1Application.java
@@ -0,0 +1,13 @@
+package com.huma.aplab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aplab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aplab1Application.class, args);
+	}
+
+}
